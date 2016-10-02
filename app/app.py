4a6ebff67f0353011c6aaa4e1d528e70e065ef2c
Joe Manley <joemanley201@gmail.com>
2016-10-02 10:08:38 -0700

    Finalize Groups

--- app/app.py
+++ app/app.py
@@ -7,30 +7,42 @@ import json
 import csv
 import io
 from analyze import analyze_csv_file
+from form_groups import form_groups
 
 app = Flask(__name__)
 
-uploaded_file = None
+uploaded_stream = None
+analyzed_result = None
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-	print request.method, "hello"
 	return render_template('index.html')
 
 @app.route("/upload_csv", methods=['GET', 'POST'])
 def upload_csv():
-	global uploaded_file
+	global uploaded_stream
+	global analyzed_result
 	uploaded_file = request.files['uploaded_file']
 
 	stream = io.StringIO(uploaded_file.stream.read().decode("UTF8"), newline=None)
+	uploaded_stream = stream
+	
 	uploaded_csv = csv.reader(stream)
 	
-	return json.dumps(analyze_csv_file(uploaded_csv))
+	analyzed_result = analyze_csv_file(uploaded_csv)
+	return json.dumps(analyzed_result)
 
 @app.route("/finalize_groups_send_mail", methods=['GET', 'POST'])
 def finalize_groups_send_mail():
-	print uploaded_file
-	return "sadas"
+	global uploaded_stream
+	global analyzed_result
+	
+	columns = ['country','university']
+	number_of_groups = 10
+
+	groups = form_groups(columns, number_of_groups, uploaded_stream, analyzed_result)
+	
+	return json.dumps(groups)
 
 if __name__ == '__main__':
 	app.run(debug=True)
--- app/form_groups.py
+++ app/form_groups.py
@@ -1,34 +1,41 @@
 import itertools
+import csv
+import random
 total_size = 500
 
 def pick_one_person(combination, fields, csvFile):
-    for row in csvFile:
-        matched = False
-        for idx in xrange(len(fields)):
-            if row[fields[idx]] != combination[idx]:
-                continue
-        email = row['email']
-        del row
-        return email
-    return None
+	fields = [3, 4]
+	for row in csvFile:
+		matched = False
+		for idx in xrange(len(fields)):
+			if row[fields[idx]] != combination[idx]:
+				continue
+		email = row[1]
+		del row
+		return email
+	return None
 
 
-def form_groups(fields, number_of_groups, csv_file):
-    headers = uploaded_csv_file.next()
+def form_groups(fields, number_of_groups, csv_file_stream, analyzed_result):
+	csv_file_stream.seek(0)
+	csv_file = csv.reader(csv_file_stream)
 
-    combinations = []
-    for field in fields:
-        combinations.append(output[field].keys())
 
-    combinations = list(itertools.product(*combinations))
-    group_size = total_size / number_of_groups
-    output_groups = []
-    for i in xrange(number_of_groups):
-        output_groups.append([])
-        for j in xrange(group_size):
-            selected_person = None
-            while selected_person == None:
-                random_group = random.randrange(0, len(combinations))
-                selected_person = pick_one_person(combinations[random_group], fields, csvFile)    
-                output_groups[i].append(selected_person)        
-    print output_groups
+	headers = csv_file.next()
+
+	combinations = []
+	for field in fields:
+		combinations.append(analyzed_result[field].keys())
+
+	combinations = list(itertools.product(*combinations))
+	group_size = total_size / number_of_groups
+	output_groups = []
+	for i in xrange(number_of_groups):
+		output_groups.append([])
+		for j in xrange(group_size):
+			selected_person = None
+			while selected_person == None:
+				random_group = random.randrange(0, len(combinations))
+				selected_person = pick_one_person(combinations[random_group], fields, csv_file)    
+				output_groups[i].append(selected_person)        
+	return output_groups
